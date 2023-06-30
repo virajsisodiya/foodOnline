ea0cdfd958327e3f2f2f27b6f1c8fbf30cd204a6
virajsisodiya <sisodiyaviraj12@gmail.com>
2023-06-30 06:27:47 +0530

    customer and vendor registration

---
+++ accounts/utils.py
@@ -0,0 +1,13 @@
+
+
+
+def detectuser(user):
+    if user.role == 1:
+        redirectUrl = 'vendorDashboard'
+        return redirectUrl
+    elif user.role == 2:
+        redirectUrl = 'custDashboard'
+        return redirectUrl
+    elif user.role == None and user.is_superadmin:
+        redirectUrl = 'admin/'
+        return redirectUrl
